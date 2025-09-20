eb24bca5abcf7d50ee0c47752b38be174f89d947
Rushikesh <rushikeshjaisur11@gmail.com>
2025-09-20 08:15:52 +0530

    Remove changes to sidekick_tools.py

--- 4_langgraph/sidekick_tools.py
+++ 4_langgraph/sidekick_tools.py
@@ -11,12 +11,12 @@ from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
 
 
+
 load_dotenv(override=True)
 pushover_token = os.getenv("PUSHOVER_TOKEN")
 pushover_user = os.getenv("PUSHOVER_USER")
 pushover_url = "https://api.pushover.net/1/messages.json"
 serper = GoogleSerperAPIWrapper()
-
 
 async def playwright_tools():
     playwright = await async_playwright().start()
@@ -27,10 +27,7 @@ async def playwright_tools():
 
 def push(text: str):
     """Send a push notification to the user"""
-    requests.post(
-        pushover_url,
-        data={"token": pushover_token, "user": pushover_user, "message": text},
-    )
+    requests.post(pushover_url, data = {"token": pushover_token, "user": pushover_user, "message": text})
     return "success"
 
 
@@ -40,22 +37,19 @@ def get_file_tools():
 
 
 async def other_tools():
-    push_tool = Tool(
-        name="send_push_notification",
-        func=push,
-        description="Use this tool when you want to send a push notification",
-    )
+    push_tool = Tool(name="send_push_notification", func=push, description="Use this tool when you want to send a push notification")
     file_tools = get_file_tools()
 
-    tool_search = Tool(
+    tool_search =Tool(
         name="search",
         func=serper.run,
-        description="Use this tool when you want to get the results of an online web search",
+        description="Use this tool when you want to get the results of an online web search"
     )
 
     wikipedia = WikipediaAPIWrapper()
     wiki_tool = WikipediaQueryRun(api_wrapper=wikipedia)
 
     python_repl = PythonREPLTool()
+    
+    return file_tools + [push_tool, tool_search, python_repl,  wiki_tool]
 
-    return file_tools + [push_tool, tool_search, python_repl, wiki_tool]

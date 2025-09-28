3483e9259aa4661679dd0793af7293ab577579e1
Eleftherios Chaniotakis <lchanio@echyperion.com>
2025-09-28 13:07:47 +0300

    Add cost tracking to Research Manager

--- 2_openai/community_contributions/elchanio76/deep_research/research_manager.py
+++ 2_openai/community_contributions/elchanio76/deep_research/research_manager.py
@@ -1,4 +1,4 @@
-from agents import Runner, trace, gen_trace_id
+from agents import Runner, trace, gen_trace_id, Usage
 from search_agent import search_agent
 from planner_agent import PlannerAgent, WebSearchItem, WebSearchPlan
 from writer_agent import writer_agent, ReportData
@@ -9,6 +9,9 @@ import asyncio
 class ResearchManager:
     def __init__(self):
         self.report: ReportData | None = None
+        self.input_tokens: int = 0
+        self.output_tokens: int = 0
+        self.cost: float = 0.0
 
     async def run(self, query: str):
         """ Run the deep research process, yielding the status updates and the final report"""
@@ -51,6 +54,13 @@ class ResearchManager:
             )
             yield result.final_output.answer
 
+    def update_usage_stats(self, usage:Usage)->None:
+        self.input_tokens += usage.input_tokens
+        self.output_tokens += usage.output_tokens
+        input_token_cost = 0.15/1000000
+        output_token_cost = 0.60/1000000
+        self.cost += usage.input_tokens*input_token_cost + usage.output_tokens*output_token_cost
+
     async def plan_searches(self, query: str, model:str="gpt-4o-mini", num_searches: int = 5) -> WebSearchPlan:
         """ Plan the searches to perform for the query """
         print("Planning searches...")
@@ -60,6 +70,8 @@ class ResearchManager:
             f"Query: {query}",
         )
         print(f"Will perform {len(result.final_output.searches)} searches")
+        self.update_usage_stats(result.context_wrapper.usage)
+        print(f"Total cost: {self.cost}")
         return result.final_output_as(WebSearchPlan)
 
     async def perform_searches(self, search_plan: WebSearchPlan) -> list[str]:
@@ -72,9 +84,13 @@ class ResearchManager:
             result = await task
             if result is not None:
                 results.append(result)
+                
             num_completed += 1
             print(f"Searching... {num_completed}/{len(tasks)} completed")
+
         print("Finished searching")
+        
+        print(f"Total cost: {self.cost}")
         return results
 
     async def search(self, item: WebSearchItem) -> str | None:
@@ -85,6 +101,10 @@ class ResearchManager:
                 search_agent,
                 input,
             )
+            self.update_usage_stats(result.context_wrapper.usage)
+            # Add the search tool cost to the total cost
+            # TODO: Find out if there's a more elegant way to do this
+            self.cost += 25/1000
             return str(result.final_output)
         except Exception:
             return None
@@ -97,8 +117,9 @@ class ResearchManager:
             writer_agent,
             input,
         )
-
+        self.update_usage_stats(result.context_wrapper.usage)
         print("Finished writing report")
+        print(f"Total cost: {self.cost}")
         return result.final_output_as(ReportData)
     
     async def send_email(self, report: ReportData) -> None:
@@ -107,5 +128,7 @@ class ResearchManager:
             email_agent,
             report.markdown_report,
         )
+        self.update_usage_stats(result.context_wrapper.usage)
         print("Email sent")
+        print(f"Total cost: {self.cost}")
         return report
